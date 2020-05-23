a5111bc87229657cc5ae9ca2df3387318ac1975f
Jensen David <feudavid92@gmail.com>
2020-05-22 20:32:09 -0700

    random RGb functionality working

--- color.js
+++ color.js
@@ -10,6 +10,33 @@ let colors = [
 
 let squares= document.querySelectorAll('.square');
 
+var pickedColor = pickColor();
+var colorDisplay = document.querySelector('#colorDisplay');
+var messageDisplay = document.querySelector('#message');
+
+
+colorDisplay.textContent =pickedColor;
+
 for(let i = 0; i < squares.length; i++){
 	squares[i].style.background = colors[i];
+	squares[i].addEventListener("click", function(){
+		let clickedColor = this.style["background-color"];
+		if(clickedColor === pickedColor){
+			messageDisplay.textContent = "Correct";
+			changeColors(clickedColor);
+		} else {
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	  });
+}
+function changeColors(color){
+	for(let i = 0; i < squares.length; i++){
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor(){
+	let random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
